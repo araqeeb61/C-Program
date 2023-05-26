9e095bb53b6000c0d15e98d460837e1a30087453
araqeeb61 <araqeeb618@gmail.com>
2023-05-26 18:17:00 +0500

    first commit

---
+++ asteric.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main (void)
+{ char i='*';
+//   shape no one 
+   puts("*");
+   puts("**");
+   puts("**");
+   puts("**");
+   puts("**");
+   puts("**");
+   puts("**");
+   puts("**");
+   puts("*");
+   puts("*********");
+   puts("*       *");
+   puts("*       *");
+   puts("*       *");
+   puts("*       *");
+   puts("*       *");
+   puts("*       *");
+   puts("*       *");
+   puts("***   * *****");
+   puts("   ****");
+   puts(" *     *");
+   puts("*       *");
+   puts("*       *");
+   puts("*       *");
+   puts("*       *");
+   puts(" *       *");
+   puts("   *     *");
+   puts("***   *");
+   puts("*");
+   puts("***");
+   puts("*****");
+   puts("*");
+   puts("*");
+   puts("*");
+   puts("*");
+   puts("*");
+   puts("*");
+    
+   return 0;
+   
+}
